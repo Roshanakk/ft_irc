7bd9770a6a6b0d87d999f8260c1c64e179b68ca4
Drew <91377105+Drewski6@users.noreply.github.com>
2024-06-26 19:46:12 +0200

    Moving serverException to it's own file

---
+++ includes/ServerException.hpp
@@ -0,0 +1,14 @@
+#include <exception>
+#include <string>
+
+class serverException : public std::exception {
+  public:
+
+    serverException(void);
+    serverException(const std::string& msg);
+    ~serverException(void) throw();
+    const char *what(void) const throw();
+
+  private:
+    std::string _msg;
+};
