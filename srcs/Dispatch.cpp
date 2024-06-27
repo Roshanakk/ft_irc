99bbc59a66ab2ee867c85303ef1ccec4c199a037
Drew <91377105+Drewski6@users.noreply.github.com>
2024-06-27 11:37:33 +0200

    moved the dispatch class to it's own header and source files

--- srcs/Dispatch.cpp
+++ srcs/Dispatch.cpp
@@ -12,7 +12,7 @@ dispatch::dispatch(void)
 ~dispatch(void) {};
 
 // Member Functions //
-void add(io_event& event) {
+void dispatch::add(io_event& event) {
   struct epoll_event ev;
 
   // ev.events = EPOLLIN | EPOLLOUT;
@@ -23,4 +23,42 @@ void add(io_event& event) {
     throw serverException("Error: failed to add event to epoll instance.");
   }
 
+}
+
+void dispatch::remove(const io_event& event) {
+  if (epoll_ctl(_epollfd, EPOLL_CTL_DEL, event.socket_func(), NULL) == -1) {
+    perror("epoll_ctl");
+    throw serverException("Error: failed to remove event from epoll instance.");
+  } 
+}
+
+void dispatch::run(void) {
+  int nfds = epoll_wait(_epollfd, _events, MAX_EVENTS, -1);
+  if (nfds == -1) {
+    perror("epoll_wait failed");
+    throw serverException("Error: failed to wait for events.");
+  }
+  std::cout << "nfds: " << nfds << std::endl;
+
+  for (int i = 0; i < nfds; ++i) {
+    std::cout << "i: " << i << std::endl;
+    std::cout << "_events[i].data.ptr: " << static_cast<io_event *>(_events[i].data.ptr) << std::endl;
+    io_event *event = static_cast<io_event *>(_events[i].data.ptr);
+    if (_events[i].events & EPOLLIN) {
+      std::cout << "calling receive_message" << std::endl;
+      std::cout << "event _type: " << event->getType() << std::endl;
+      std::cout << "fd for reading: " << event->socket_func() << std::endl;
+      event->receive_message();
+    }
+    if (_events[i].events & EPOLLOUT) {
+      std::cout << "calling send_message" << std::endl;
+      std::cout << "event _type: " << event->getType() << std::endl;
+      std::cout << "fd for writing: " << event->socket_func() << std::endl;
+      event->send_message();
+    }
+  }
+}
+
+int dispatch::get_epollfd(void) {
+  return _epollfd;
 }
