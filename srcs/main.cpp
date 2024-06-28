c3d6bc9e6d2263a18bd3272fa4cfeb5703d37ae4
Drew <91377105+Drewski6@users.noreply.github.com>
2024-06-28 15:42:55 +0200

    Adding print statement for port and pass in main so we can see what's entered. Debug only, remove later

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,14 +6,21 @@
 
 int main(int argc, char **argv) {
 
+  int port = 0;
+  std::string passStr;
+
   try {
     ArgParse::numArgs(argc);
-    ArgParse::parsePort(argv[1]);
-    ArgParse::parsePass(argv[2]);
+    port = ArgParse::parsePort(argv[1]);
+    passStr = ArgParse::parsePass(argv[2]);
   } catch (const ServerException& e) {
     std::cerr << e.what() << std::endl;
     return 1;
   }
+
+  // DEBUG: show port and password
+  std::cout << "Port: " << port << std::endl;
+  std::cout << "Password: " << passStr << std::endl;
 
   Dispatch d;
   Server s(d);
