db49021a382f14b7afed25eafef93ac75c999364
Drew <91377105+Drewski6@users.noreply.github.com>
2024-07-22 14:40:07 +0200

    Finished. works now with make re and make fun

--- srcs/Command.cpp
+++ srcs/Command.cpp
@@ -834,7 +834,9 @@ _____________________________________ \n\
 |    |     |      _|_ |  \\ \\__.     |\n\
 |___________________________________|\n\
  \n\
-|" << BLUE << "OOOOOOOOOOOOOOO" << WHITE << "OOOOOOOOOOOOOOO" << RED << "OOOOOOOOOOOOOOO" << RESET << "| |" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << RED << "OOOOOOOOOOOOOOOOOOOOOOOOO" << RESET << "|\n\
+";
+#ifdef FUN_FLAGS
+ss << "|" << BLUE << "OOOOOOOOOOOOOOO" << WHITE << "OOOOOOOOOOOOOOO" << RED << "OOOOOOOOOOOOOOO" << RESET << "| |" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << RED << "OOOOOOOOOOOOOOOOOOOOOOOOO" << RESET << "|\n\
 |" << BLUE << "OOOOOOOOOOOOOOO" << WHITE << "OOOOOOOOOOOOOOO" << RED << "OOOOOOOOOOOOOOO" << RESET << "| |" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << BLUE << "@" << WHITE << "OOOOOOOOOOOOOOOOOOOOOOOOO" << RESET << "|\n\
 |" << BLUE << "OOOOOOOOOOOOOOO" << WHITE << "OOOOOOOOOOOOOOO" << RED << "OOOOOOOOOOOOOOO" << RESET << "| |" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << RED << "OOOOOOOOOOOOOOOOOOOOOOOOO" << RESET << "|\n\
 |" << BLUE << "OOOOOOOOOOOOOOO" << WHITE << "OOOOOOOOOOOOOOO" << RED << "OOOOOOOOOOOOOOO" << RESET << "| |" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << WHITE << "*" << BLUE << "@" << BLUE << "@" << WHITE << "OOOOOOOOOOOOOOOOOOOOOOOOO" << RESET << "|\n\
@@ -846,9 +848,9 @@ _____________________________________ \n\
 |" << BLUE << "OOOOOOOOOOOOOOO" << WHITE << "OOOOOOOOOOOOOOO" << RED << "OOOOOOOOOOOOOOO" << RESET << "| |" << WHITE << "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO" << RESET << "|\n\
 |" << BLUE << "OOOOOOOOOOOOOOO" << WHITE << "OOOOOOOOOOOOOOO" << RED << "OOOOOOOOOOOOOOO" << RESET << "| |" << RED << "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO" << RESET << "|\n\
 |" << BLUE << "OOOOOOOOOOOOOOO" << WHITE << "OOOOOOOOOOOOOOO" << RED << "OOOOOOOOOOOOOOO" << RESET << "| |" << WHITE << "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO" << RESET << "|\n\
-|" << BLUE << "OOOOOOOOOOOOOOO" << WHITE << "OOOOOOOOOOOOOOO" << RED << "OOOOOOOOOOOOOOO" << RESET << "| |" << RED << "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO" << RESET << "|\n\
- \n\
-This server is capable of handling the following commands:\n\
+|" << BLUE << "OOOOOOOOOOOOOOO" << WHITE << "OOOOOOOOOOOOOOO" << RED << "OOOOOOOOOOOOOOO" << RESET << "| |" << RED << "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO" << RESET << "|\n \n";
+#endif
+    ss << "This server is capable of handling the following commands:\n\
 CAP, INFO, INVITE, JOIN, LIST, KICK, KILL, MODE, NAMES, NICK, NOTICE, PART, PASS, PING, PRIVMSG, QUIT, TOPIC, USER, VERSION, WHO\n\
  \n\
 The server is also capable of handling the following modes:\n\
