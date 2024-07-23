28d011f394f03591a9cc977409c2e9a30cb9039b
Drew <91377105+Drewski6@users.noreply.github.com>
2024-07-23 17:55:19 +0200

    NOT TESTED: changed a lot of macros so that the hostname of the server and the error code is give with all the errors.

--- includes/ServerReplies.hpp
+++ includes/ServerReplies.hpp
@@ -1,46 +1,45 @@
 /************************************************/
 // Error Replies (Section 6.1 of RFC 1459)
 /************************************************/
-#define ERR_NOSUCHNICK(nick)                (nick + " :No such nick/channel\r\n")
-#define ERR_NOSUCHSERVER(nick, servername)  (":ft_irc 402 " + nick + " " + servername + " :No such server\r\n")
-#define ERR_NOSUCHCHANNEL(nick, channel)    (":ft_irc 403 " + nick + " " + channel + " :No such channel\r\n")
-#define ERR_CANNOTSENDTOCHAN(nick, channel) (":ft_irc 404 " + nick + " " + channel + " :Cannot send to channel\r\n")
-#define ERR_TOOMANYCHANNELS(channel)        (channel + " :You have joined too many channels\r\n")
-#define ERR_WASNOSUCHNICK(nick)             (nick + " :There was no such nickname\r\n")
-#define ERR_TOOMANYTARGETS(target)          (target + " :Duplicate recipients. No message delivered\r\n")
-#define ERR_NOORIGIN()                      (":No origin specified\r\n")
-#define ERR_NORECIPIENT(command)            (":No recipient given (" + command + ")\r\n")
-#define ERR_NOTEXTTOSEND()                  (":No text to send\r\n")
-#define ERR_NOTOPLEVEL(domain)              (domain + " :No toplevel domain specified\r\n")
-#define ERR_WILDTOPLEVEL(domain)            (domain + " :Wildcard in toplevel domain\r\n")
-#define ERR_UNKNOWNCOMMAND(hostname, cmd)   (cmd + " :" + "Unknown command\r\n") 
-#define ERR_NOMOTD()                        (":MOTD File is missing\r\n")
-#define ERR_NOADMININFO(servername)         (servername + " :No administrative info available\r\n")
-#define ERR_FILEERROR(fileop, file)         (":File error doing " + fileop + " on " + file + "\r\n")
-#define ERR_NONICKNAMEGIVEN()               (":No nickname given\r\n")
-#define ERR_ERRONEUSNICKNAME(nick)          (nick + " :Erroneus nickname\r\n")
-#define ERR_NICKNAMEINUSE(nick)             (nick + " :Nickname is already in use\r\n")
-#define ERR_NICKCOLLISION(nick)             (nick + " :Nickname collision KILL\r\n")
-#define ERR_USERNOTINCHANNEL(nick, channel) (nick + " " + channel + " :They aren't on that channel\r\n")
-#define ERR_NOTONCHANNEL(channel)           (channel + " :You're not on that channel\r\n")
-#define ERR_USERONCHANNEL(nick, channel)    (nick + " " + channel + " :is already on channel\r\n")
-#define ERR_NOLOGIN(nick)                   (nick + " :User not logged in\r\n")
-#define ERR_SUMMONDISABLED()                (":SUMMON has been disabled\r\n")
-#define ERR_USERSDISABLED()                 (":USERS has been disabled\r\n")
-#define ERR_NOTREGISTERED()                 (":You have not registered\r\n")
-#define ERR_NEEDMOREPARAMS(nick, command)   (":ft_irc 461 " + nick + " " + command + " :Not enough parameters\r\n")
-#define ERR_ALREADYREGISTRED()              (":You may not reregister\r\n")
-#define ERR_NOPERMFORHOST()                 (":Your host isn't among the privileged\r\n")
-#define ERR_PASSWDMISMATCH()                (":Password incorrect\r\n")
-#define ERR_PASSWDNEEDED()                  ("Password needed\r\n")
-#define ERR_YOUREBANNEDCREEP()              (":You are banned from this server\r\n")
-#define ERR_KEYSET(channel)                 (channel + " :Channel key already set\r\n")
-#define ERR_CHANNELISFULL(nick, channel)    (":ft_irc 471 " + nick + " " + channel + " :Cannot join channel (+l)\r\n")
-#define ERR_UNKNOWNMODE(nick, mode)         (":ft_irc 472 " + nick + " " + mode + " :is not a recognised channel mode.\r\n")
-#define ERR_INVITEONLYCHAN(nick, channel)   (":ft_irc 473 " + nick + " " + channel + " :Cannot join channel (+i)\r\n")
-#define ERR_BANNEDFROMCHAN(channel)         (channel + " :Cannot join channel (+b)\r\n")
-#define ERR_BADCHANNELKEY(user, channel)    (":ft_irc 475 " + user + " " + channel + " :Cannot join channel (incorrect channel key)\r\n")
-#define ERR_NOPRIVILEGES()                  (":Permission Denied- You're not an IRC operator\r\n")
+#define ERR_NOSUCHNICK(nick)                      (":ft_irc 401 " + nick + " :No such nick/channel\r\n")
+#define ERR_NOSUCHSERVER(nick, servername)        (":ft_irc 402 " + nick + " " + servername + " :No such server\r\n")
+#define ERR_NOSUCHCHANNEL(nick, channel)          (":ft_irc 403 " + nick + " " + channel + " :No such channel\r\n")
+#define ERR_CANNOTSENDTOCHAN(nick, channel)       (":ft_irc 404 " + nick + " " + channel + " :Cannot send to channel\r\n")
+#define ERR_TOOMANYCHANNELS(nick, channel)        (":ft_irc 405 " + nick + " " + channel + " :You have joined too many channels\r\n")
+#define ERR_WASNOSUCHNICK(nick)                   (":ft_irc 406 " + nick + " :There was no such nickname\r\n")
+#define ERR_TOOMANYTARGETS(nick, target)          (":ft_irc 407 " + nick + " " + target + " :Duplicate recipients. No message delivered\r\n")
+#define ERR_NOORIGIN(nick)                        (":ft_irc 409 " + nick + " :No origin specified\r\n")
+#define ERR_NORECIPIENT(nick, command)            (":ft_irc 411 " + nick + " :No recipient given (" + command + ")\r\n")
+#define ERR_NOTEXTTOSEND(nick)                    (":ft_irc 412 " + nick + " :No text to send\r\n")
+#define ERR_NOTOPLEVEL(nick, domain)              (":ft_irc 413 " + nick + " " + domain + " :No toplevel domain specified\r\n")
+#define ERR_WILDTOPLEVEL(nick, domain)            (":ft_irc 414 " + nick + " " + domain + " :Wildcard in toplevel domain\r\n")
+#define ERR_UNKNOWNCOMMAND(nick, hostname, cmd)   (":ft_irc 421 " + nick + " " + cmd + " :Unknown command\r\n") 
+#define ERR_NOMOTD(nick)                          (":ft_irc 422 " + nick + " :MOTD File is missing\r\n")
+#define ERR_NOADMININFO(nick, servername)         (":ft_irc 423 " + nick + " " + servername + " :No administrative info available\r\n")
+#define ERR_FILEERROR(nick, fileop, file)         (":ft_irc 424 " + nick + " :File error doing " + fileop + " on " + file + "\r\n")
+#define ERR_NONICKNAMEGIVEN(nick)                 (":ft_irc 431 " + nick + " :No nickname given\r\n")
+#define ERR_ERRONEUSNICKNAME(nick, nick2)         (":ft_irc 432 " + nick + " " + nick2 + " :Erroneus nickname\r\n")
+#define ERR_NICKNAMEINUSE(nick)                   (":ft_irc 433 " + nick + " :Nickname is already in use\r\n")
+#define ERR_NICKCOLLISION(nick, nick2)            (":ft_irc 436 " + nick + " " + nick2 + " :Nickname collision KILL\r\n")
+#define ERR_USERNOTINCHANNEL(nick, nick2, channel)(":ft_irc 441 " + nick + " " + nick2 + " " + channel + " :They aren't on that channel\r\n")
+#define ERR_NOTONCHANNEL(nick, channel)           (":ft_irc 442 " + nick + " " + channel + " :You're not on that channel\r\n")
+#define ERR_USERONCHANNEL(nick, nick2, channel)   (":ft_irc 443 " + nick + " " + nick2 + " " + channel + " :is already on channel\r\n")
+#define ERR_NOLOGIN(nick, nick2)                  (":ft_irc 444 " + nick + " " + nick2 + " :User not logged in\r\n")
+#define ERR_USERSDISABLED(nick)                   (":ft_irc 446 " + nick + " :USERS has been disabled\r\n")
+#define ERR_NOTREGISTERED()                       (":ft_irc 451 :You have not registered\r\n")
+#define ERR_NEEDMOREPARAMS(nick, command)         (":ft_irc 461 " + nick + " " + command + " :Not enough parameters\r\n")
+#define ERR_ALREADYREGISTRED(nick)                (":ft_irc 462 " + nick + " :You may not reregister\r\n")
+#define ERR_NOPERMFORHOST(nick)                   (":ft_irc 463 " + nick + " :Your host isn't among the privileged\r\n")
+#define ERR_PASSWDMISMATCH()                      (":Password incorrect\r\n")
+#define ERR_PASSWDNEEDED()                        (":Password needed\r\n")
+#define ERR_YOUREBANNEDCREEP(nick)                (":ft_irc 465 " + nick + " :You are banned from this server\r\n")
+#define ERR_KEYSET(nick, channel)                 (":ft_irc 467 " + nick + " " + channel + " :Channel key already set\r\n")
+#define ERR_CHANNELISFULL(nick, channel)          (":ft_irc 471 " + nick + " " + channel + " :Cannot join channel (+l)\r\n")
+#define ERR_UNKNOWNMODE(nick, mode)               (":ft_irc 472 " + nick + " " + mode + " :is not a recognised channel mode.\r\n")
+#define ERR_INVITEONLYCHAN(nick, channel)         (":ft_irc 473 " + nick + " " + channel + " :Cannot join channel (+i)\r\n")
+#define ERR_BANNEDFROMCHAN(nick, channel)         (":ft_irc 474 " + nick + " " + channel + " :Cannot join channel (+b)\r\n")
+#define ERR_BADCHANNELKEY(user, channel)          (":ft_irc 475 " + user + " " + channel + " :Cannot join channel (incorrect channel key)\r\n")
+#define ERR_NOPRIVILEGES()                        (":ft_irc 481 " + user + " :Permission Denied- You're not an IRC operator\r\n")
 // freenode uses multiple 482 responses, so we will use the same format
 // Also note that if we include the nick in the response before the channel, we will kick the user from the channel.
 #define ERR_CHANOPRIVSNEEDED_gen(nick, channel)   (":ft_irc 482 " + channel + " :You must have channel operator status to set or unset channel mode\r\n")
@@ -49,14 +48,11 @@
 #define ERR_CHANOPRIVSNEEDED_kick(nick, channel)  (":ft_irc 482 " + channel + " :You do not have the right to kick users\r\n")
 // fix for part not making irssi leave the channel
 #define ERR_CHANOPRIVSNEEDED_part(nick, channel)  (":ft_irc 482 " + nick + " " + channel + " :part\r\n")
-
 #define ERR_CHANOPRIVSNEEDED(nick, channel)       (":ft_irc 482 " + nick + " " + channel + " :You're not channel operator\r\n")
-
-#define ERR_CANTKILLSERVER()                      (":You cant kill a server!\r\n")
-#define ERR_NOOPERHOST()                          (":No O-lines for your host\r\n")
-#define ERR_UMODEUNKNOWNFLAG()                    (":Unknown MODE flag\r\n")
-#define ERR_USERSDONTMATCH()                      (":Cant change mode for other users\r\n")
-#define ERR_RESTRICTED()                          (":Your connection is restricted!")
+#define ERR_CANTKILLSERVER(nick)                  (":ft_irc 483 " + nick + " :You cant kill a server!\r\n")
+#define ERR_NOOPERHOST(nick)                      (":ft_irc 491 " + nick + " :No O-lines for your host\r\n")
+#define ERR_UMODEUNKNOWNFLAG(nick)                (":ft_irc 501 " + nick + " :Unknown MODE flag\r\n")
+#define ERR_USERSDONTMATCH(nick)                  (":ft_irc 502 " + nick + " :Cant change mode for other users\r\n")
 
 /****************************************************************/
 // Command Responses (Section 6.2 of RFC 1459)
@@ -65,53 +61,26 @@
 /****************************************************************/
 
 // Connection Confirmation (Not in RFC 1459)
-#define RPL_WELCOME(hostname, nick, prefix)                     (":" + hostname + " 001 " + nick + " :Welcome to the Internet Relay Network " + prefix + "\r\n")
+#define RPL_WELCOME(nick, userPrefix)                           (":ft_irc 001 " + nick + " :Welcome to the Internet Relay Network" + userPrefix + "\r\n")
 // #define RPL_YOURHOST() "002 :Your host is <servername>, running version <ver>"
 // #define RPL_CREATED() "003 This server was created <date>"
 // #define RPL_MYINFO() "004 RPL_MYINFO <servername> <version> <available user modes> <available channel modes>"NB_CMDS
-#define RPL_TOPIC(prefix, channel, topic) (prefix + " TOPIC " + channel + " :" + topic + "\r\n")
-#define RPL_NOTOPIC(channel) (":ft_irc 331 " + channel + " :No topic is set\r\n")
-#define RPL_NAMREPLY(nick, channel, listNicks) (":ft_irc 353 " + nick + " = " + channel + " :" + listNicks + "\r\n")
-#define RPL_NICK(prefix, nickname) (prefix + " NICK " + nickname + "\r\n")
-#define RPL_KICK(user_prefix, channel, kicked, reason) (user_prefix + " KICK " + channel + " " + kicked + " " + reason + "\r\n")
+#define RPL_TOPIC(prefix, channel, topic)                       (prefix + " TOPIC " + channel + " :" + topic + "\r\n")
+#define RPL_NOTOPIC(channel)                                    (":ft_irc 331 " + channel + " :No topic is set\r\n")
+#define RPL_NAMREPLY(nick, channel, listNicks)                  (":ft_irc 353 " + nick + " = " + channel + " :" + listNicks + "\r\n")
+#define RPL_NICK(prefix, nickname)                              (prefix + " NICK " + nickname + "\r\n")
+#define RPL_KICK(user_prefix, channel, kicked, reason)          (user_prefix + " KICK " + channel + " " + kicked + " " + reason + "\r\n")
 #define RPL_WHOWASUSER(nickname, whowasNick, whowasHost, realname) ("ft_irc: 314 " + nickname + " " + whowasNick + " " + whowasHost + " :" + realname + "\r\n")
-
 #define RPL_WHOIS(nickname, takenNick, username, hostname, realname) (":ft_irc 311 " + nickname + " " + takenNick + " " + username + " " + hostname + " * :" + realname + "\r\n")
-
-#define RPL_USERHOST(nickname, takenNick) (":ft_irc 312 " + nickname + " " + takenNick + " ft_irc :ft_irc\r\n")
-#define RPL_ENDOFWHOIS(nickname, takenNick) (":ft_irc 318 " + nickname + " " + takenNick + " :End of /WHOIS list.\r\n")
-#define RPL_TOPICUSAGE(nick) (":ft_irc 650 " + nick + " TOPIC :<channel> [:<topic>]\r\n")
-
-// #define RPL_INVITE(userPrefix, userToInvite, channel) (userPrefix + " INVITE " + userToInvite + " " + channel + "\r\n")
-// #define RPL_INVITING(hostname, user, channel, userToInvite) (":" + hostname + " 341 " + user + " " + userToInvite + " " + channel + "\r\n")
-
-
-// #define RPL_INVITE(userPrefix, userToInvite, channel) (userPrefix + " INVITE " + userToInvite + " :" + channel + "\r\n")
-// #define RPL_INVITING(hostname, user, channel, userToInvite) (":" + hostname + " 341 " + user + " " + userToInvite + " :" + channel + "\r\n")
-
-//inviter : nini
-//invited : jojo
-
-// >> :*.freenode.net 341 nini jojo :#disney
-// :hostname 341 inviter invited :channel
-
-// >> :nini!~rraffi-k@freenode-3dg.s40.6vib9m.IP INVITE jojo :#disney
-// prefox INVITE invited :channel
-
-// #define RPL_INVITING(userPrefix, user, userToInvite, channel) (userPrefix + " 341 " + user + " " + userToInvite + " " + channel + "\r\n")
-// #define RPL_INVITE(channel, userToInvite) (":ft_irc 341 " + userToInvite + "\r\n")
-
-#define RPL_INVITING(user, userToInvite, channel) (":ft_irc 341 " + user + " " + userToInvite + " " + channel + "\r\n")
-#define RPL_INVITE(userPrefix, userToInvite, channel) (userPrefix + " INVITE " + userToInvite + " " + channel + "\r\n")
-
-
-
-
-#define RPL_ENDOFWHOWAS(nickname) (nickname + " :End of WHOWAS\r\n")
-#define RPL_VERSION(version, hostname, comments) (":ft_irc 351 " + version + " " + hostname + " :" + comments + "\r\n")
-#define RPL_ENDOFNAMES(nick, channel) (":ft_irc 366 " + nick + " " + channel + " :End of /NAMES list.\r\n")
-// >> :*.freenode.net 324 yo1 #heythereguys :+nt$
-#define RPL_CHANNELMODEIS(nick, channel, mode) (":ft_irc 324 " + nick + " " + channel + " :" + mode + "\r\n")
+#define RPL_USERHOST(nickname, takenNick)                       (":ft_irc 312 " + nickname + " " + takenNick + " ft_irc :ft_irc\r\n")
+#define RPL_ENDOFWHOIS(nickname, takenNick)                     (":ft_irc 318 " + nickname + " " + takenNick + " :End of /WHOIS list.\r\n")
+#define RPL_TOPICUSAGE(nick)                                    (":ft_irc 650 " + nick + " TOPIC :<channel> [:<topic>]\r\n")
+#define RPL_INVITING(user, userToInvite, channel)               (":ft_irc 341 " + user + " " + userToInvite + " " + channel + "\r\n")
+#define RPL_INVITE(userPrefix, userToInvite, channel)           (userPrefix + " INVITE " + userToInvite + " " + channel + "\r\n")
+#define RPL_ENDOFWHOWAS(nickname)                               (nickname + " :End of WHOWAS\r\n")
+#define RPL_VERSION(version, hostname, comments)                (":ft_irc 351 " + version + " " + hostname + " :" + comments + "\r\n")
+#define RPL_ENDOFNAMES(nick, channel)                           (":ft_irc 366 " + nick + " " + channel + " :End of /NAMES list.\r\n")
+#define RPL_CHANNELMODEIS(nick, channel, mode)                  (":ft_irc 324 " + nick + " " + channel + " :" + mode + "\r\n")
 #define RPL_TOPIC(prefix, channel, topic)                       (prefix + " TOPIC " + channel + " :" + topic + "\r\n")
 #define RPL_NOTOPIC(channel)                                    (":ft_irc 331 " + channel + " :No topic is set\r\n")
 #define RPL_NAMREPLY(nick, channel, listNicks)                  (":ft_irc 353 " + nick + " = " + channel + " :" + listNicks + "\r\n")
--- srcs/Command.cpp
+++ srcs/Command.cpp
@@ -60,7 +60,7 @@ void Command::doCmd(std::string & line)
 			++i;
 
         if (i == NB_CMDS)
-            throw(CommandException(ERR_UNKNOWNCOMMAND(_client.getHostname(), _cmd)));
+            throw(CommandException(ERR_UNKNOWNCOMMAND(_client.getNickname(), _client.getHostname(), _cmd)));
         if (_client.getStatus() != PASS_REGISTERED && _cmd != "PASS" && _cmd != "NICK" && _cmd != "USER") {
             if (_client.getStatus() == PASS_NEEDED)
                 throw(CommandException(ERR_PASSWDNEEDED()));
@@ -114,13 +114,13 @@ void Command::handle_INVITE()
 		throw(CommandException(ERR_NOSUCHCHANNEL(_client.getNickname(), channelName)));
 	
 	if (!channel->checkIfClientInChannel(&_client))
-		throw(CommandException(ERR_NOTONCHANNEL(channelName)));
+		throw(CommandException(ERR_NOTONCHANNEL(_client.getNickname(), channelName)));
 	
 	if (!channel->checkIfClientOperator(&_client))
 		throw(CommandException(ERR_CHANOPRIVSNEEDED(_client.getPrefix(), channelName)));
 	
 	if (channel->checkIfClientInChannel(userToInvite))
-		throw(CommandException(ERR_USERONCHANNEL(userToInviteName, channelName)));
+		throw(CommandException(ERR_USERONCHANNEL(_client.getNickname(), userToInviteName, channelName)));
 
 	_client.send_message(RPL_INVITING(_client.getNickname(), userToInviteName, channelName));
 	userToInvite->send_message(RPL_INVITE(_client.getPrefix(), userToInviteName, channelName));
@@ -146,7 +146,7 @@ void Command::handle_JOIN() {
 
         // Check that user is not in too many channels
         if (cm.getClientChannelCount(&_client) >= cm.getMaxChannelsForClient()) {
-            throw CommandException(ERR_TOOMANYCHANNELS(channelsVec[i]));
+            throw CommandException(ERR_TOOMANYCHANNELS(_client.getNickname(), channelsVec[i]));
         }
 
         // Following checks are only required if the channel exists
@@ -241,7 +241,7 @@ void Command::handle_KICK()
 	if (_parameters.empty())
 		throw(CommandException(ERR_NEEDMOREPARAMS(_client.getNickname(), _cmd)));
 	if (paramChannelNames.empty() || paramUsername.empty())
-		throw(CommandException(ERR_NOSUCHNICK(_parameters[0])));
+		throw(CommandException(ERR_NOSUCHNICK(paramUsername)));
 
 
 	// Checking if ALL given channel names exist
@@ -281,7 +281,7 @@ void Command::handle_KICK()
 		if (!paramChannel->checkIfClientOperator(&_client))
 			throw(CommandException(ERR_CHANOPRIVSNEEDED_kick(_client.getNickname(), paramChannel->getName())));
 		if (!paramChannel->checkIfClientInChannel(paramUser))
-			throw(CommandException(ERR_USERNOTINCHANNEL(paramUsername, paramChannel->getName())));
+			throw(CommandException(ERR_USERNOTINCHANNEL(_client.getNickname(), paramUsername, paramChannel->getName())));
 
 		// paramChannel->banUser(paramUser);
 		_client.send_message(RPL_KICK(_client.getPrefix(), paramChannel->getName(), paramUser->getNickname(),reason));
@@ -326,7 +326,7 @@ void Command::handle_MODE() {
     if (paramsVec.size() < 2)
         throw CommandException(RPL_CHANNELMODEIS(_client.getNickname(), chan->getName(), chan->getMode()));
     if (!chan->checkIfClientInChannel(&_client))
-        throw CommandException(ERR_NOTONCHANNEL(paramsVec[0]));
+        throw CommandException(ERR_NOTONCHANNEL(_client.getNickname(), paramsVec[0]));
     
     // Check if the mode is a ban mode request (we only repond to irssi during join)
     size_t pos = paramsVec[1].find('b');
@@ -458,14 +458,14 @@ void Command::handle_NICK()
 		throw(CommandException(ERR_PASSWDNEEDED()));
 
 	if (_client.getNickname().empty() && params.size() < 1)
-		throw(CommandException(ERR_NONICKNAMEGIVEN()));
+		throw(CommandException(ERR_NONICKNAMEGIVEN(_client.getNickname())));
 	else if (params.size() < 1)
         _client.send_message(RPL_NICK(oldPrefix, _client.getNickname()));
 	else if (params.size() == 1)
 	{
 		std::string nickname = params[0];
 		if (!isAValidNickname(nickname))
-			throw(CommandException(ERR_ERRONEUSNICKNAME(nickname)));
+			throw(CommandException(ERR_ERRONEUSNICKNAME(_client.getNickname(), nickname)));
 
 		std::set<Client *> & allClients = _client.getClients();
 		std::set<Client *>::iterator it = allClients.begin();
@@ -497,7 +497,7 @@ void Command::handle_NICK()
 		if (_client.isAuth())
 		{
 			_client.setStatus(PASS_REGISTERED);
-			_client.send_message(RPL_WELCOME(_client.getHostname(), _client.getNickname(), _client.getPrefix()));
+			_client.send_message(RPL_WELCOME(_client.getNickname(), _client.getPrefix()));
             funWelcomeMessage();
 		}
 	}
@@ -521,7 +521,7 @@ void Command::handle_PART() {
         if (chan->checkIfClientInChannel(&_client)) {
             chan->removeClient(&_client);
         } else {
-            throw CommandException(ERR_NOTONCHANNEL(params[0]));
+            throw CommandException(ERR_NOTONCHANNEL(_client.getNickname(), params[0]));
         }
     } else {
         throw CommandException(ERR_NOSUCHCHANNEL(_client.getNickname(), params[0]));
@@ -552,7 +552,7 @@ void Command::handle_PART() {
 void Command::handle_PASS()
 {
 	if (_client.isAuth())
-		throw(CommandException(ERR_ALREADYREGISTRED()));
+		throw(CommandException(ERR_ALREADYREGISTRED(_client.getNickname())));
 
 	std::vector<std::string> params = Utilities::split(_parameters, ' ');
 
@@ -575,7 +575,7 @@ void Command::handle_PASS()
 		if (_client.isAuth()) {
 
         }
-			_client.send_message(RPL_WELCOME(_client.getHostname(), _client.getNickname(), _client.getPrefix()));
+			_client.send_message(RPL_WELCOME(_client.getNickname(), _client.getPrefix()));
 	}
 }
 
@@ -672,7 +672,7 @@ void Command::handle_TOPIC()
 	else //if there is a topic parameter
 	{
 		if (!channel->checkIfClientInChannel(&_client))
-			throw(CommandException(ERR_NOTONCHANNEL(channel->getName())));
+			throw(CommandException(ERR_NOTONCHANNEL(_client.getNickname(), channel->getName())));
 
 		if (channel->getOnlyOperTopic()
             || (channel->onlyOperCanChangeTopic() && !channel->checkIfClientOperator(&_client)))
@@ -700,7 +700,7 @@ void Command::handle_USER()
 		throw(CommandException(ERR_PASSWDNEEDED()));
 
 	if (_client.isAuth())
-		throw(CommandException(ERR_ALREADYREGISTRED()));
+		throw(CommandException(ERR_ALREADYREGISTRED(_client.getNickname())));
 
 	size_t colonPos = _parameters.find(':');
 	std::string tmpParams = _parameters.substr(0, colonPos);
@@ -732,7 +732,7 @@ void Command::handle_USER()
 		if (_client.isAuth())
 		{
 			_client.setStatus(PASS_REGISTERED);
-			_client.send_message(RPL_WELCOME(_client.getHostname(), _client.getNickname(), _client.getPrefix()));
+			_client.send_message(RPL_WELCOME(_client.getNickname(), _client.getPrefix()));
             funWelcomeMessage();
 		}
 	}
@@ -760,7 +760,7 @@ void Command::handle_WHO() {
     if (chan == NULL)
         throw CommandException(ERR_NOSUCHCHANNEL(_client.getNickname(), _parameters));
     if (!chan->checkIfClientInChannel(&_client))
-        throw CommandException(ERR_NOTONCHANNEL(_parameters));
+        throw CommandException(ERR_NOTONCHANNEL(_client.getNickname(), _parameters));
 
     for (std::map<Client*, bool>::iterator it = chan->getClients().begin(); it != chan->getClients().end(); ++it) {
         _client.send_message(RPL_WHOREPLY(  it->first->getNickname(), 
